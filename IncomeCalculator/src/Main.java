8a086ba10e13da90738434f447225801d95cd062
viago2k20 <denzh@mail.ru>
2020-01-10 01:40:23 +0300

    formula instead of array

--- IncomeCalculator/src/Main.java
+++ IncomeCalculator/src/Main.java
@@ -2,8 +2,7 @@ import org.w3c.dom.ls.LSOutput;
 
 import java.util.Scanner;
 
-public class Main
-{
+public class Main {
     private static int minIncome = 200000;//минимальная сумма дохода компании
     private static int maxIncome = 900000;//максимальная сумма дохода компании
 
@@ -19,51 +18,37 @@ public class Main
 
     private static double minInvestmentsAmount = 100000;//минимальная сумма инвестирования
 
-       public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         //расчет минимальной суммы дохода для инвестирования
-          double i;
-//        double t = 0;
-//        double m;
-//        while (t < 1)
-//        {
-//            i++;
-//            m = i*managerPercent;
-//            t = i/(calculateFixedCharges()+m+(i-m-calculateFixedCharges())*mainTaxPercent + minInvestmentsAmount);
-//            //(calculateFixedCharges()+m+(i-m-calculateFixedCharges())*mainTaxPercent + minInvestmentsAmount)=i;
-//            //calculateFixedCharges()+ i*managerPercent+i*mainTaxPercent-i*managerPercent*mainTaxPercent-calculateFixedCharges()*mainTaxPercent+minInvestmentsAmount=i
-//            //calculateFixedCharges()-calculateFixedCharges()*mainTaxPercent+minInvestmentsAmount=i-i*managerPercent-i*mainTaxPercent+i*managerPercent*mainTaxPercent
-//            //calculateFixedCharges()-calculateFixedCharges()*mainTaxPercent+minInvestmentsAmount=i*(1-managerPercent-mainTaxPercent+managerPercent*mainTaxPercent)
-//            //i=(calculateFixedCharges()-calculateFixedCharges()*mainTaxPercent+minInvestmentsAmount)/(1-managerPercent-mainTaxPercent+managerPercent*mainTaxPercent)
-//        }
-        i=(calculateFixedCharges()-calculateFixedCharges()*mainTaxPercent+minInvestmentsAmount)/(1-managerPercent-mainTaxPercent+managerPercent*mainTaxPercent);
+        double i;
+
+        i = (calculateFixedCharges() - calculateFixedCharges() * mainTaxPercent + minInvestmentsAmount) / (1 - managerPercent - mainTaxPercent + managerPercent * mainTaxPercent);
         System.out.println("Минимальная сумма дохода для инвестирования: " + i);
 
-        while(true)
-        {
+        while (true) {
             System.out.println("Введите сумму доходов компании за месяц " +
-                "(от 200 до 900 тысяч рублей): ");
+                    "(от 200 до 900 тысяч рублей): ");
             int income = (new Scanner(System.in)).nextInt();
 
-            if(!checkIncomeRange(income)) {
+            if (!checkIncomeRange(income)) {
                 continue;
             }
 
             double managerSalary = income * managerPercent;//вычисляет зарплаты менеджера
             double pureIncome = income - managerSalary -
-                calculateFixedCharges();//вычисляет чистый доход
+                    calculateFixedCharges();//вычисляет чистый доход
             double taxAmount = mainTaxPercent * pureIncome;//вычисляет общую сумму налога
             double pureIncomeAfterTax = pureIncome - taxAmount;//вычисляет доход после вычета налога
 
             boolean canMakeInvestments = pureIncomeAfterTax >=
-                minInvestmentsAmount;//условие при котором компания может инвестировать
+                    minInvestmentsAmount;//условие при котором компания может инвестировать
 
             System.out.println("Зарплата менеджера: " + managerSalary);//выводит в консоль зарплату менеджера
             System.out.println("Общая сумма налогов: " +
-                (taxAmount > 0 ? taxAmount : 0));//выводит в консоль общую сумму налогов
+                    (taxAmount > 0 ? taxAmount : 0));//выводит в консоль общую сумму налогов
             System.out.println("Компания может инвестировать: " +
-                (canMakeInvestments ? "да" : "нет"));//выводит в консоль может ли компания инвестировать
-            if(pureIncome < 0) {
+                    (canMakeInvestments ? "да" : "нет"));//выводит в консоль может ли компания инвестировать
+            if (pureIncome < 0) {
                 System.out.println("Бюджет в минусе! Нужно срочно зарабатывать!");
             }
         }
@@ -71,13 +56,11 @@ public class Main
 
     private static boolean checkIncomeRange(int income)//метод, который проверяет меньше или больше введенная сумма указанных границ
     {
-        if(income < minIncome)
-        {
+        if (income < minIncome) {
             System.out.println("Доход меньше нижней границы");
             return false;
         }
-        if(income > maxIncome)
-        {
+        if (income > maxIncome) {
             System.out.println("Доход выше верхней границы");
             return false;
         }
