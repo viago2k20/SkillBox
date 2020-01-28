19117a4abe9f3a97282223572cd3835f247b003b
viago2k20 <denzh@mail.ru>
2020-01-29 01:14:37 +0300

    count Planes

--- Module 3/Planes/src/Main.java
+++ Module 3/Planes/src/Main.java
@@ -4,6 +4,7 @@ import com.skillbox.airport.Flight;
 import com.skillbox.airport.Terminal;
 
 import java.util.*;
+import java.util.concurrent.SynchronousQueue;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +12,27 @@ public class Main {
 
         System.out.println("\nВсе самолеты в аэропорту: " + airport.getAllAircrafts().toString());
         System.out.println(airport.getTerminals());
+        String str = airport.getAllAircrafts().toString();
+
+        int total = 0;
+        for (int j=0; j<str.length();j++){
+            if (str.charAt(j) == ','){
+                total ++;
+            }
+        }
+        System.out.println("Количество самолетов: " + (total+1));
+
+        int count = 0;
+
+        String pattern = ",\\s";
+        String[] myArr = str.split(pattern);
+        for (String i : myArr) {
+            count++;
+        }
+        System.out.println("Количесвто самолетов: " + count);
+
+        Object[] arr = airport.getAllAircrafts().toArray();
+        System.out.println("Количество самолетов: " + arr.length);
 
 
         //создаем новый терминал
