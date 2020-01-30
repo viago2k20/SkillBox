082eb7c7c401139f3394cd831e6b68dc8b3f45f8
viago2k20 <denzh@mail.ru>
2020-01-30 20:14:51 +0300

    sum of digits

--- Module 4/IntegerExperiments/src/Main.java
+++ Module 4/IntegerExperiments/src/Main.java
@@ -3,39 +3,21 @@ public class Main {
         Container container = new Container();
         container.count += 7843;
 
-        System.out.println(sumDigits(83720));
-
-
-        System.out.println("\n---");
-        int num = Integer.parseInt("09");
-        System.out.println(num);
-
-        String s = " 1234 ";
-        System.out.println(s.trim());
-
-        int i = Integer.parseInt (s.trim());
-        System.out.println(i);
-        i+=16;
-        System.out.println(i);
+        System.out.println("Сумма цифр в числе " + container.count + " равна " + sumDigits(container.count));
 
     }// end of main
 
     public static Integer sumDigits(Integer number) {
         //TODO: write code here
-        String str;
-        str = Integer.toString(number);
         int result = 0;
+        String str = Integer.toString(number);
 
-        System.out.println("Длина строки: " + str.length());
         String nStr = (str.charAt(0) + "");//преобразуем символ в строку добавлением пустой строки ""
 
-        System.out.println("Первый символ: " + nStr);
-
         for (int i = 0; i < str.length(); i++) {
-            result += Integer.parseInt(str.charAt(i) + "");//
+            result += Integer.parseInt(str.charAt(i) + "");
         }
-        System.out.print("Сумма цифр в числе равна: ");
 
         return result;
-    }
+    }//end of sumDigits()
 }// end of class
