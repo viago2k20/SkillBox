734ae786e3d857ec9ca04714cc4a4976dad4ad1c
viago2k20 <denzh@mail.ru>
2020-02-02 01:26:35 +0300

    something new

--- Module 4/IntegerExperiments/src/Main.java
+++ Module 4/IntegerExperiments/src/Main.java
@@ -1,9 +1,12 @@
+
 public class Main {
     public static void main(String[] args) {
         Container container = new Container();
-        container.count += 7843;
+        container.count += 784335;
 
         System.out.println("\nСумма цифр в числе " + container.count + " равна " + sumDigits(container.count));
+        System.out.println("\nСумма цифр в числе " + container.count + " равна " + sumDigitsChar(container.count));
+
 
     }// end of main
 
@@ -13,9 +16,22 @@ public class Main {
         String str = Integer.toString(number);
 
         for (int i = 0; i < str.length(); i++) {
-            result += Integer.parseInt(str.charAt(i) + "");
+            result += Integer.parseInt(Character.toString(str.charAt(i)));
+        }
+        return result;
+    }// end of sumDigits
+
+    public static Integer sumDigitsChar(Integer number) {
+        //TODO: write next code here
+        int total = 0;
+
+        char[] chars = number.toString().toCharArray();
+
+        for (char i : chars) {
+            total += Integer.parseInt(Character.toString(i), 10);
         }
 
-        return result;
-    }//end of sumDigits()
+        return total;
+    }// end of sumDigitsChar
+
 }// end of class
