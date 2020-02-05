e78763b7d014e1c2a83a24d899b021c570b3dd23
viago2k20 <denzh@mail.ru>
2020-02-05 23:39:25 +0300

    add new project

--- Module 4/Cargos/src/Main.java
+++ Module 4/Cargos/src/Main.java
@@ -6,29 +6,29 @@ public class Main {
         int boxToTank = 27; // кол-во ящиков помещающихся в контейнер
         int box = 657; //кол-во ящиков
 
-        double tanks = Math.ceil((double) box / boxToTank);
-        double cargos = Math.ceil(tanks / tankToCargo);
+        double tanks = Math.ceil((double) box / boxToTank);//расчет кол-ва контейнеров
+        double cargos = Math.ceil(tanks / tankToCargo);//расчет кол-ва грузовиков
 
         System.out.println("\nКол-во ящиков для траспортировки " + box + " шт.");
-        System.out.println("Необходимое кол-во контейнеров для перевозки ящиков " + (int)tanks + " шт.");
-        System.out.println("Необходимое кол-во грузовиков "+ (int)cargos + " шт.");
+        System.out.println("Необходимое кол-во контейнеров для перевозки ящиков " + (int) tanks + " шт.");
+        System.out.println("Необходимое кол-во грузовиков " + (int) cargos + " шт.");
         System.out.println("\nРаспределение ящиков по контейнерам и контейнеров по грузовикам: \n");
 
         int countTank = 1;//счетчик контейнеров
         int countBox = 1;//счетчик ящиков
 
         for (int i = 1; i <= cargos; i++) {
-            System.out.println("Грузовик " + i);
+            System.out.println("Грузовик: " + i);
             for (int j = 1; j <= tankToCargo; j++) {
                 if (countTank <= tanks) {
-                    System.out.print("\tКонтейнер " + countTank + "\n");
+                    System.out.print("\tКонтейнер: " + countTank + "\n");
                     countTank++;
-                } else break;//System.out.println("\txx");
+                } else break;
                 for (int x = 1; x <= boxToTank; x++) {
-                    if(countBox <= box){
-                        System.out.print("\t\tЯщик " + countBox + "\n");
-                    countBox++;
-                    }else break;//System.out.println("\t\t00");
+                    if (countBox <= box) {
+                        System.out.print("\t\tЯщик: " + countBox + "\n");
+                        countBox++;
+                    } else break;
                 }
             }
             System.out.println("");
