3c63e301126d4059ee8adae8d8373eab191833dc
viago2k20 <denzh@mail.ru>
2020-02-18 00:11:32 +0300

    add changes to Letters

--- Module 4/Letters/src/Main.java
+++ Module 4/Letters/src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
-    static void printLetters (String str){
-        for (int i = 0; i <= str.length() - 1; i++) {
-            int code = str.charAt(i);
-            System.out.println(str.charAt(i) + "\t" + code);
-        }
-        System.out.println("Кол-во символов " + str.length() + "\n");
-    }
 
     public static void main(String[] args) {
-        String alphabetL = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String alphabetR = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-        String numbers = "0123456789";
 
-        printLetters(alphabetL);
-        printLetters(alphabetR);
-        printLetters(numbers);
+        for (char i = 'A'; i <= 'Z'; i++) {
+            System.out.println(i + "\t" + (int) i);
+        }
+        System.out.println("--------");
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.println(i + "\t" + (int) i);
+        }
+        System.out.println("--------");
+        for (char x = 'А'; x <= 'я'; x++) {
+            System.out.println(x + "\t" + (int) x);
+        }
+        System.out.println("--------");
+        for (char x = '0'; x <= '9'; x++) {
+            System.out.println(x + "\t" + (int) x);
+        }
 
-        System.out.println("-> " + (char)50);
-        System.out.println("-> " + (int)'?');
     }
 }
