f1836de1874ed28430011060a5152fd3964fbaa7
viago2k20 <denzh@mail.ru>
2020-02-27 19:52:15 +0300

    add some changes to SplitText

--- Module 4/SplitText/src/Main.java
+++ Module 4/SplitText/src/Main.java
@@ -7,11 +7,12 @@ public class Main {
                 "A further 52,000 customers were told that less sensitive personal information was exposed. This was only a portion of those impacted.\n" +
                 "MGM said its notification to customers followed state laws. Most US states do not require companies to tell customers if data which is already public has been exposed during a hack.\n" +
                 "MGM has resorts in Las Vegas, Atlantic City and Detroit in the US. It also has property in China and Japan and is developing a new resort in Dubai.";
-        String pattern = "\\s+";
-        String[] msg = txt.replaceAll("[^([0-9],|.[0-9]+\\w)]", " ").split(pattern);
-        for(int i=0; i<msg.length; i++) {
 
-            System.out.println(msg[i].replaceAll("(,$|\\.$)", ""));
+        String pattern = "[.,]?\\s+";
+        String[] msg = txt.replaceAll("[^([0-9],|.[0-9]+\\w)]|(\\.$)", " ").split(pattern);
+
+        for(int i=0; i<msg.length; i++) {
+            System.out.println(msg[i]);
         }
         System.out.println("\n"+msg.length);
     }
